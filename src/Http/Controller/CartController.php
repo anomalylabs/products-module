ceaf0669d9ff4cd9db28f3516f98001cbd728bef
Ryan Thompson <ryan@pyrocms.com>
2017-03-25 12:08:21 -0500

    Redirects arenice

--- src/Http/Controller/CartController.php
+++ src/Http/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 use Anomaly\ProductsModule\Product\Contract\ProductRepositoryInterface;
 use Anomaly\Streams\Platform\Http\Controller\PublicController;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class CartController
@@ -17,16 +18,23 @@ class CartController extends PublicController
      * Add a product to the cart.
      *
      * @param ProductRepositoryInterface $products
+     * @return array|\Illuminate\Http\RedirectResponse|null
      */
     public function add(ProductRepositoryInterface $products)
     {
-        $this->events->fire(
+        $response = $this->events->fire(
             'store::cart.add',
             [
                 $products->first(),
                 $this->request->get('quantity', 1),
             ]
         );
+
+        $response = end($response);
+
+        if ($response instanceof Response) {
+            return $response;
+        }
 
         return $this->redirect->route('store::cart.view');
     }
